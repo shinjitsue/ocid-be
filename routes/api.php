996d9107877ef1e35b067e00ea59fafbd775712a
shinjitsue <jorenverdad84@gmail.com>
2025-07-08 15:04:56 +0800

    feat: Implement authentication using Laravel Sanctum

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+});
